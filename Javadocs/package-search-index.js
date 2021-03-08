d5e07b0f97cfb9021c07a942ba210acd1218c188
Sifaldin Abbas <57807968+Sifaldin@users.noreply.github.com>
2021-03-08 09:03:23 +0100

    Delete Javadocs directory
    Delete generated docs

--- Javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"IP.TodoListApplication"},{"l":"IP.TodoListApplication.App"},{"l":"IP.TodoListApplication.DataSorting"},{"l":"IP.TodoListApplication.Features"},{"l":"IP.TodoListApplication.SaveRead"}]
